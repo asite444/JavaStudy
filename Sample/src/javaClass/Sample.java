467ec0a1c637dfcac003507884901962d2ce2023
site4 <site4@LIMBOOK>
2023-11-08 12:11:43 +0900

    sample

--- Sample/src/javaClass/Sample.java
+++ Sample/src/javaClass/Sample.java
@@ -4,14 +4,11 @@ import java.util.Arrays;
 
 public class Sample {
 
+	
+	
 	public static void main(String[] args) {
-		int arr[]= {10,20,30,40,50,60};
-		int arra[]=Arrays.copyOfRange(arr, 0, 3);
-		int arrb[]=Arrays.copyOfRange(arr, 3, arr.length);
 		
-		for(int i:arra) System.out.print(i+" ");
-		System.out.println();
-		for(int i:arrb) System.out.print(i+" ");
+		
 		
 		
 		
