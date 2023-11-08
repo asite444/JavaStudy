24edb49e05c889ccb074eebfe969baff15469a57
site4 <site4@LIMBOOK>
2023-11-08 16:03:19 +0900

    ㅌ

--- Sample/src/timeComplexity/BinarySearchAlgorithm.java
+++ Sample/src/timeComplexity/BinarySearchAlgorithm.java
@@ -20,18 +20,18 @@ public class BinarySearchAlgorithm {
 
 	// O(log n)
 	public static int binarySearch(int[] nums, int target) {
-	    int left = 0, right = nums.length - 1;
-	    while (left <= right) {
-	        int mid = left + (right - left) / 2;
-	        if (nums[mid] == target) {
-	            return mid;
-	        } else if (nums[mid] < target) {
-	            left = mid + 1;
-	        } else {
-	            right = mid - 1;
-	        }
-	    }
-	    return -1;
+		int left = 0, right = nums.length - 1;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (nums[mid] == target) {
+				return mid;
+			} else if (nums[mid] < target) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		return -1;
 	}
-	
+
 }
